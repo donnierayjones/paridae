1a4dcd50b93d2da2311a699a85fd30f9b250b2f0
Donnie Ray Jones <donnierayjones@gmail.com>
2012-03-07 21:11:41 -0600

    twitter window size now persisted on close

--- background.js
+++ background.js
@@ -14,12 +14,14 @@
   };
 
   var getWindowDimensions = function() {
-    return {
-      width: localStorage[STOR_WIDTH_KEY],
-      height: localStorage[STOR_HEIGHT_KEY],
-      top: localStorage[STOR_TOP_KEY],
-      left: localStorage[STOR_LEFT_KEY]
-    };
+    if(localStorage[STOR_WIDTH_KEY] !== undefined) {
+      return {
+        width: localStorage[STOR_WIDTH_KEY],
+        height: localStorage[STOR_HEIGHT_KEY],
+        top: localStorage[STOR_TOP_KEY],
+        left: localStorage[STOR_LEFT_KEY]
+      };
+    }
   };
 
   // note: "best" is currently defined as the one with the largest width
@@ -67,23 +69,33 @@
 
   // open twitter.com in a (mostly) chrome-less window
 
-  // default dimensions
-  saveWindowDimensions({ width: 430, height: 620, top: 0, left: 0 });
-
   chrome.browserAction.onClicked.addListener(function(tab) {
     getExistingTwitterWindow(function(existingWindow) {
       if(existingWindow !== undefined) {
         chrome.windows.update(existingWindow.id, { focused: true });
       } else {
         var dimensions = getWindowDimensions();
-        window.open(
-          TWITTER_URL,
-          'winname',
-          'toolbar=no,location=no,width=' + dimensions.width + ',height=' + dimensions.height + ',top=' + dimensions.top + ',left=' + dimensions.left
-        );
+        var window_options = 'toolbar=no,location=no';
 
-        // TODO: Monitor the window for resize and save it to localStorage
-        // i.e. window.onresize = saveWindowDimensions();
+        if(dimensions !== undefined) {
+          window_options +=
+            ',width=' + dimensions.width +
+            ',height=' + dimensions.height +
+            ',top=' + dimensions.top +
+            ',left=' + dimensions.left;
+        }
+
+        window.open(TWITTER_URL, 'winname', window_options);
+
+        // Monitor focus changes to persist window size. The Chrome API does
+        // not offer onResize event, so this is the closest we can get without
+        // a content script sending messages to the extension.
+
+        chrome.windows.onFocusChanged.addListener(function(windowId) {
+          getExistingTwitterWindow(function(twitterWindow) {
+            saveWindowDimensions(twitterWindow);
+          });
+        });
       }
     });
   });
